9e7ec466a8a81928b692ecf0d9d714bd72bbd15f
Daniel Morgan <dmorgan@digital-restart.com>
2018-06-26 19:32:42 -0400

    Won't render Artist List...

---
+++ app/src/main/java/com/example/raiff/latemusicialstructurepart2/ArtistAdapter.java
@@ -0,0 +1,39 @@
+package com.example.raiff.latemusicialstructurepart2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+public class ArtistAdapter extends ArrayAdapter<Artist> {
+    public ArtistAdapter(Context context, ArrayList<Artist> artists) {
+        super(context, 0, artists);
+    }
+    @Override
+    public View getView(int  position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Artist currentArtist = getItem(position);
+
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.default_imageView);
+
+        imageView.setImageResource(currentArtist.getArtistImage());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+
+        defaultTextView.setText(currentArtist.getArtist());
+
+        return listItemView;
+    }
+
+}
---
+++ app/src/main/java/com/example/raiff/latemusicialstructurepart2/Content.java
@@ -0,0 +1,4 @@
+package com.example.raiff.latemusicialstructurepart2;
+
+public class Content {
+}
